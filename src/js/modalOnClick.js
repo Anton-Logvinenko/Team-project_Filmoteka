b57857e734ae2affd558a70e0dc95605d2b419c3
Anton Logvinenko <loav2236@gmail.com>
2023-04-13 16:16:35 +0300

    1

--- src/js/modalOnClick.js
+++ src/js/modalOnClick.js
@@ -121,7 +121,6 @@ function modalClose() {
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
-
   refs.closeModalBtn.addEventListener('click', toggleModal);
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
